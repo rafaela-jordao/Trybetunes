9512be750404a417c9feccabebaaefa5fb201828
Rafaela Jordão <rafa.jordao.07@hotmail.com>
2022-03-08 17:32:07 -0300

    requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      btnDisabled: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const caracter = 2;
+    this.setState(() => ({
+      name: target.value,
+      btnDisabled: target.value.length < caracter,
+    }));
+  }
+
   render() {
+    const { name, btnDisabled } = this.state;
+
     return (
       <div data-testid="page-search">
-        Search
+        <form>
+          <label htmlFor="search">
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              name="search"
+              id="search"
+              placeholder="Nome do artista"
+              onChange={ this.handleChange }
+              value={ name }
+            />
+            <button
+              data-testid="search-artist-button"
+              type="button"
+              id="search-button"
+              disabled={ btnDisabled }
+            >
+              Pesquisar
+            </button>
+
+          </label>
+        </form>
       </div>
     );
   }
